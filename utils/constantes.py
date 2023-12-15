d70cf6836a5e92caae8b204b98c8c1057c364001
Rodrigo Arribas <rodrigoarribaslozano@gmail.com>
2023-12-15 18:23:14 +0100

    first commit

---
+++ utils/constantes.py
@@ -0,0 +1,56 @@
+# Correspondencia Owners - int
+owner_dict = {'0\xa0..\xa020,000' : 10000,
+            '20,000\xa0..\xa050,000' : 35000,
+            '50,000\xa0..\xa0100,000' : 75000,
+            '100,000\xa0..\xa0200,000' : 150000,
+            '200,000\xa0..\xa0500,000' : 350000,
+            '500,000\xa0..\xa01,000,000' : 750000,
+            '1,000,000\xa0..\xa02,000,000' : 1500000,
+            '2,000,000\xa0..\xa05,000,000' : 3500000,
+            '5,000,000\xa0..\xa010,000,000' : 7500000,
+            '10,000,000\xa0..\xa020,000,000' : 15000000,
+            '20,000,000\xa0..\xa050,000,000' : 35000000,
+            '50,000,000\xa0..\xa0100,000,000' : 75000000,
+            '100,000,000\xa0..\xa0200,000,000' : 150000000,
+            '200,000,000\xa0..\xa0500,000,000' : 350000000}
+
+
+# Etiquetas
+top_level_genres = ['Action','Adventure','Casual','Experimental','Puzzle','Racing','RPG','Simulation','Sports','Strategy','Tabletop']
+genres = ['Action RPG','Action-Adventure','Arcade','Auto Battler','Automobile Sim','Base Building','Baseball','Basketball','Battle Royale',
+          'BMX','Board Game','Bowling','Building','Card Game','Character Action Game','Chess','Clicker','Cycling','Diplomacy','eSports','Experimental',
+          'Exploration','Farming Sim','Fighting','Football','God Game','Golf','Hacking','Hidden Object','Hockey','Idler','Interactive Fiction'
+          'Management','Match 3','Medical Sim','Mini Golf','Mining','MMORPG','MOBA','Motocross','Open World','Outbreak Sim','Party-Based RPG',
+          'Pinball','Platformer','Point & Click','Rhythm','Roguelike','RTS','Sandbox','Shooter','Skateboarding','Skating','Skiing','Snowboarding',
+          'Soccer','Space Sim','Stealth','Strategy RPG','Survival','Tennis','Tower Defense','Trivia','Turn-Based Strategy','Visual Novel',
+          'Walking Simulator','Word Game','Wrestling']
+subgenres = ['2D Fighter','2D Platformer','3D Fighter','3D Platformer','4X','Action Roguelike','Arena Shooter',"Beat 'em up",'Bullet Hell',
+             'Card Battler','Choose Your Own Adventure','City Builder','Collectathon','Colony Sim','Combat Racing','CRPG','Dating Sim',
+             'Dungeon Crawler','Education','Flight','FPS','Grand Strategy','Hack and Slash','Heist','Hero Shooter','Horror','Immersive Sim',
+             'Investigation','JRPG','Life Sim','Looter Shooter','Metroidvania','Mystery Dungeon','On-Rails Shooter','Open World Survival Craft',
+             'Political Sim','Precision Platformer','Programming','Real Time Tactics','Roguelite','Roguevania','Runner',"Shoot 'Em Up",'Side Scroller',
+             'Sokoban','Solitaire','Souls-like','Spectacle fighter','Spelling','Survival Horror','Tactical RPG','Third-Person Shooter',
+             'Time Management','Top-Down Shooter','Trading','Trading Card Game','Traditional Roguelike','Turn-Based Tactics','Twin Stick Shooter','Typing','Wargame']
+visuals = ['2.5D','2D','360 Video','3D','3D Vision','Abstract','Anime','Cartoon','Cartoony','Cinematic','Colorful','Comic Book',
+           'Cute','First-Person','FMV','Hand-drawn','Isometric','Minimalist','Noir','Pixel Graphics','Psychedelic','Realistic',
+           'Split Screen','Stylized','Text-Based','Third Person','Top-Down','Voxel','VR']
+players = ['4 Player Local','Asynchronous Multiplayer','Co-op','Co-op Campaign','Local Co-Op','Local Multiplayer','Massively Multiplayer',
+           'Multiplayer'',Online Co-Op','Singleplayer']
+
+
+# Paleta de colores Steam, fuente: https://colorswall.com/palette/193
+c1 = '#171a21'
+c2 = '#66c0f4'
+c3 = '#1b2838'
+c4 = '#2a475e' #el más representativo
+c5 = '#c7d5e0'
+steam_palette = [c1,c2,c3,c4,c5]
+
+
+# Paleta de colores Midnight Barber
+mb1 = "#c99a54"
+mb2 = "#8895be"
+mb3 = "#4f2e3e"
+mb4 = "#c7d5f5"
+mb5 = "#3e325b"
+midnight_palette = [mb1,mb2,mb3,mb4,mb5]
